916dc162785f7ceb6e1d51518848e305c65feaa1
Yaşar Ünyılmaz <116540999+yasarunylmzz@users.noreply.github.com>
2025-05-20 13:51:55 +0300

    refactor: Update feature file path in TestRunner and clean up unused imports in BrowserUtils

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "/Users/yasarunyilmaz/IdeaProjects/com.testcase/src/test/java/resource/features/Case3_CriticalPath.feature",
+        features = "src/test/java/resource/features/Case3_CriticalPath.feature",
         glue = {"steps","hooks"},                            
         plugin = {
                 "pretty",
--- src/test/java/utilities/BrowserUtils.java
+++ src/test/java/utilities/BrowserUtils.java
@@ -1,12 +1,9 @@
 package utilities;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
 
 public class BrowserUtils {
